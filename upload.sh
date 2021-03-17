30710549c754c57c97a71433430aeff1d943cf81
JR Castillo <jr.yonzon@icloud.com>
2021-03-17 12:05:04 +0800

    Error handling in duplicated layers.

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
+
 #! /usr/bin/bash
 
 read -p "GeoServer Username: " username
@@ -6,22 +7,18 @@ read -s password
 
 import_id=$(curl -s -u $username:$password -XPOST -H "Content-type: application/json" -d @import.json "http://192.168.6.113/geoserver/rest/imports" | jq '.import.id')
 
-# import_state=$(curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.state')
-
-# echo $import_id
-# echo $import_state
-
-# if [[ $import_state = PENDING ]]; then
-
 echo "Initiating an import to GeoServer and preparing tasks."
-echo "STATUS:"
-curl -s -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.href, .import .state'
 curl -s -u $username:$password -XPOST "http://192.168.6.113/geoserver/rest/imports/{$import_id}"
 
-echo "Importing data files to GeoServer."
-curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.'
+import_state=$(curl -s -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import | .state')
+task_state=$(curl -s -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.tasks[0] | .state')
 
-# fi
-
-echo "Updating GeoNode Layers."
-docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered
+if [[ ${import_state} = '"PENDING"' && ${task_state} = '"ERROR"' ]];
+then
+        echo "Error in uploading layers. (Duplicated Layers)"
+else
+        echo "Importing data files to GeoServer."
+        curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.'
+        echo "Updating GeoNode Layers."
+        docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered
+fi
