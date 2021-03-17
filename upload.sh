6455e32a2808a754d3a407445757af7ee4cb15f0
JR Castillo <jr.yonzon@icloud.com>
2021-03-17 09:23:25 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,22 +1,27 @@
 #! /usr/bin/bash
 
 read -p "GeoServer Username: " username
-echo "GeoServer Password: " 
+echo "GeoServer Password: "
 read -s password
 
-curl -u $username:$password -XPOST -H "Content-type: application/json" -d @import.json "http://192.168.6.113/geoserver/rest/imports"
-echo ""
-curl -u $username:$password -XPOST "http://192.168.6.113/geoserver/rest/imports/$id"
-echo ""
-curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/"
-echo ""
+import_id=$(curl -s -u $username:$password -XPOST -H "Content-type: application/json" -d @import.json "http://192.168.6.113/geoserver/rest/imports" | jq '.import.id')
 
-docker exec -it d379f67a81ef bash
-$HOME/.override_env; \
-    echo DATABASE_URL=$DATABASE_URL; \
-    echo GEODATABASE_URL=$GEODATABASE_URL; \
-    echo SITEURL=$SITEURL; \
-    echo ALLOWED_HOSTS=$ALLOWED_HOSTS; \
-    echo GEOSERVER_PUBLIC_LOCATION=$GEOSERVER_PUBLIC_LOCATION; \
-    django-admin updatelayers --skip-geonode-registered
-# geonode-upload-script
+# import_state=$(curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.state')
+
+# echo $import_id
+# echo $import_state
+
+# if [[ $import_state = PENDING ]]; then
+
+echo "Initiating an import to GeoServer and preparing tasks."
+echo "STATUS:"
+curl -s -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.href, .import .state'
+curl -s -u $username:$password -XPOST "http://192.168.6.113/geoserver/rest/imports/{$import_id}"
+
+echo "Importing data files to GeoServer."
+curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.'
+
+# fi
+
+echo "Updating GeoNode Layers."
+docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered
