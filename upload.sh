20a57e510169cf1227cb3654e1ccf62ae9e6f283
JR Castillo <jr.yonzon@icloud.com>
2021-03-19 11:20:22 +0800

    Adding script for updating metadata

--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 NOCOLOR='\033[0m'
 RED='\033[1;31m'
 GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 LIGHTBLUE='\033[1;34m'
+CYAN='\033[0;36m'
 
 read -p "GeoServer Username: " username;
 read -sp "GeoServer Password: " password;
@@ -10,8 +12,18 @@ read -sp "GeoServer Password: " password;
 # while read username password; do
 # curl -iL --data-urlencode  username=$username --data-urlencode password=$password http://192.168.6.113/geoserver/web
 # done
+# docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered -i --remove-deleted --traceback
 
 import_id=$(curl -s -u $username:$password -XPOST -H "Content-type: application/json" -d @import.json "http://192.168.6.113/geoserver/rest/imports" | jq '.import.id')
+task_id=$(curl -s -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.import.tasks[] | .id')
+
+for i in ${task_id[@]};
+do
+        if [[ ${task_id} = 0 || ${task_id} > 0 ]];
+        then
+                curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}/tasks/$i/layer/" | jq '.'
+        fi
+done
 
 echo -e "\n${GREEN}Initiating an import to GeoServer and preparing tasks.${NOCOLOR}"
 curl -s -u $username:$password -XPOST "http://192.168.6.113/geoserver/rest/imports/{$import_id}"
@@ -21,25 +33,24 @@ task_state=$(curl -s -u $username:$password -XGET "http://192.168.6.113/geoserve
 
 for i in ${task_state[@]};
 do
-echo ${task_state[@]}
         if [[ ${import_state} = '"PENDING"' && ${task_state[@]} =~ '"COMPLETE"' ]];
         then
-                echo -e "${RED}Some layers are duplicated.${NOCOLOR}"
-                echo -e "Uploading not existing layers."
+                echo -e "${RED}Some layers are already existing in GeoServer.${NOCOLOR}"
+                echo -e "${ORANGE}Uploading not existing layers.${NOCOLOR}"
                 echo -e "${GREEN}Importing data files to GeoServer.${NOCOLOR}\n"
                 curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '. | .import.tasks[] | select(.state == "COMPLETE")'
-                echo -e "\n${LIGHTBLUE}Updating GeoNode Layers.${NOCOLOR}"
-                docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered
+                echo -e "\n${CYAN}Fetching data from GeoServer to GeoNode.\nUpdating GeoNode Layers.${NOCOLOR}"
+                docker exec -it d379f67a81ef python manage.py updatelayers -v 3 --traceback --ignore-errors
                 break
         elif [[ ${import_state} = '"PENDING"' && ${task_state[@]} != '"ERROR"' ]];
         then
-                echo -e "${RED}Error in uploading layers.\nAll layers are duplicated${NOCOLOR}"
+                echo -e "${RED}Error in uploading layers.\nAll layers are existing in GeoServer.${NOCOLOR}"
                 break
         else
                 echo -e "${GREEN}Importing data files to GeoServer.${NOCOLOR}\n"
                 curl -u $username:$password -XGET "http://192.168.6.113/geoserver/rest/imports/{$import_id}" | jq '.'
-                echo -e "\n${LIGHTBLUE}Updating GeoNode Layers.${NOCOLOR}"
-                docker exec -it d379f67a81ef python manage.py updatelayers --skip-geonode-registered
+                echo -e "\n${CYAN}Fetching data from GeoServer to GeoNode.\nUpdating GeoNode Layers.${NOCOLOR}"
+                docker exec -it d379f67a81ef python manage.py updatelayers -v 3 --traceback --ignore-errors
                 break
         fi
 done
